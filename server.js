a1ea6ba80bb87612de2809ffe18b91a1d31786dc
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-05 12:57:02 +0200

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(port);
+var gzippo = require('gzippo');
+  var express = require('express');
+  var app = express();
+ 
+  app.use(express.logger('dev'));
+  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+  app.listen(process.env.PORT || 5000);
