f55458035fb5aa7f618bc2e4a5c8b2f11455ca66
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 14:20:03 +0200

    try

--- server.js
+++ server.js
@@ -1,15 +1,80 @@
-var express = require('express');
-var http = require('http');
-var app = express();
-app.use(express.static(__dirname + '/dist'));
-var server = http.createServer(app);
+var parseUrl = require('url').parse;
+var assign = require('lodash.assign');
 
-app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']=='https')
-    res.redirect('http://rapidmatches.herokuapp.com'+req.url)
-  else
-    next() /* Continue to other routes if we're not redirecting */
-})
+function isSecure (secure, xfpHeader, trustXFPHeader) {
+  xfpHeader = xfpHeader ? xfpHeader.toString().toLowerCase() : '';
+  if (!secure) {
+    return true;
+  }
 
-var port = process.env.PORT ||3000;
-app.listen(port);
+  return trustXFPHeader && xfpHeader === 'https'
+}
+
+function shouldRedirect (redirectsEnabled, method) {
+  if (!redirectsEnabled) {
+    return false
+  }
+
+  return method === "GET";
+}
+
+function checkForDeprecation (appSettings, optionsHttpsPort) {
+  var httpsPort = appSettings.get('httpsPort');
+
+  if (appSettings.get('env') === 'development') {
+    if (httpsPort) {
+      console.warn('express-force-ssl deprecated: app.set("httpsPort", ' + httpsPort + '), use ' +
+        'app.set("forceSSLOptions", { httpsPort: ' + httpsPort + ' }) instead.');
+    }
+
+    if (httpsPort && optionsHttpsPort) {
+      console.warn('You have set both app.get("httpsPort") and app.get("forceSSLOptions").httpsPort ' +
+        'Your app will use the value in forceSSLOptions.');
+    }
+  }
+}
+
+module.exports = function(req, res, next){
+  var redirect;
+  var secure;
+  var xfpHeader = req.get('X-Forwarded-Proto');
+  var localHttpsPort;
+  var appHttpsPort = req.app.get('httpsPort');
+  var httpsPort;
+  var fullUrl;
+  var redirectUrl;
+
+  var options = {
+    trustXFPHeader: false,
+    enable301Redirects: true,
+    httpsPort: false,
+    sslRequiredMessage: 'SSL Required.'
+  };
+
+  var expressOptions = req.app.get('forceSSLOptions') || {};
+  var localOptions = res.locals.forceSSLOptions || {};
+  localHttpsPort = localOptions.httpsPort;
+  assign(options, expressOptions, localOptions);
+
+  secure = isSecure(req.secure, xfpHeader, options.trustXFPHeader);
+  redirect = shouldRedirect(options.enable301Redirects, req.method);
+
+  if (!secure) {
+    if (redirect) {
+      checkForDeprecation(req.app, options.httpsPort);
+
+      httpsPort = localHttpsPort || options.httpsPort || appHttpsPort || 443;
+      fullUrl = parseUrl(req.protocol + '://' + req.header('Host') + req.originalUrl);
+
+      //intentionally allow coercion of https port
+      redirectUrl = 'http://' + fullUrl.hostname + (httpsPort == 80 ? '' : (':' + httpsPort)) + req.originalUrl;
+
+      res.redirect(301, redirectUrl);
+    } else {
+      res.status(403).send(options.sslRequiredMessage);
+    }
+  } else {
+    delete res.locals.forceSSLOptions;
+    next();
+  }
+};
