8c9450716142e01136ab5667affce1dca7ec8e55
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 14:10:47 +0200

    try

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-var server = http.createServer(app);
 app.use(express.static(__dirname + '/dist'));
+var server = http.createServer(app);
+
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']=='https')
+    res.redirect('http://rapidmatches.herokuapp.com'+req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
+
 var port = process.env.PORT ||3000;
 app.listen(80);
