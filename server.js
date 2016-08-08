c3ae408d239661514c10dc4dda9cc8d669f2fc17
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 14:02:10 +0200

    try

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var app = express.createServer();
+/*var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
 app.use(express.static(__dirname + '/dist'));
-app.get('*', function(req,res) {
-	if(req.protocol == 'https') {
-		return res.status(403).send({message: 'SSL not supported'});
-	}
-	next();
-});
-app.listen(80);
-
+app.listen(8000);
 
 var https = require('https');
 https.createServer(function(req,res) {
 	res.writeHead(301, {"Location": "http://" + req.headers['host'] + req.url });
 	res.end;
+}).listen(4433);
+*/
+
+var https = require('https');
+var options = {};
+https.createServer(options, function (req, res) {
+    res.end('secure!');
 }).listen(443);
+
+// Redirect from http port 80 to https
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
