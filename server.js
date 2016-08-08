972c8af5ef92c5c3fb75f10d7dad2f3893660ca6
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 14:06:00 +0200

    try

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
-/*var express = require('express');
+var express = require('express');
 var http = require('http');
 var app = express();
 var server = http.createServer(app);
 app.use(express.static(__dirname + '/dist'));
-app.listen(8000);
-
-var https = require('https');
-https.createServer(function(req,res) {
-	res.writeHead(301, {"Location": "http://" + req.headers['host'] + req.url });
-	res.end;
-}).listen(4433);
-*/
-
-var https = require('https');
-var options = {};
-https.createServer(options, function (req, res) {
-    res.end('secure!');
-}).listen(443);
-
-// Redirect from http port 80 to https
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
+app.listen(80);
