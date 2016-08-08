bba8636792d9d4ac573a50b83a6310717bb26e62
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 13:50:48 +0200

    try

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+var app = express.createServer();
 app.use(express.static(__dirname + '/dist'));
-/*app.listen(port);*/
-
-app.use(function(req, res, next) {
-    if(req.protocol !== 'https') {
-        return res.status(403).send({message: 'SSL required'});
-    }
-    next();
+app.get('*', function(req,res) {
+	if(req.protocol == 'https') {
+		return res.status(403).send({message: 'SSL not supported'});
+	}
+	next();
 });
-app.listen(port);
+app.listen(80);
+
+
+var https = require('https');
+https.createServer(function(req,res) {
+	res.writeHead(301, {"Location": "http://" + req.headers['host'] + req.url });
+	res.end;
+}).listen(443);
