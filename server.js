f4be05b9184bbd32bd550c6b378af9513f5880d5
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 14:09:06 +0200

    try

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 app.use(express.static(__dirname + '/dist'));
+var port = process.env.PORT ||3000;
 app.listen(80);
