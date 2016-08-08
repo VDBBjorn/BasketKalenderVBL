f9d13fa00d76fb02c0789e2efc801c0df25a5a41
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-08-08 13:31:08 +0200

    redirect to http

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
-var http = require('http');
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/dist'));
 /*app.listen(port);*/
-http.createServer(app).listen(port);
+
+function requireHttp(req, res, next) {
+	if(req.secure) {
+		return res.redirect('http://' + req.get('host') + req.url);
+	}
+	next();
+}
+
+app.use(requireHttp);
+app.listen(port);
